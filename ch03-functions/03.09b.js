02759a663e9569f295a781c85b65bacb061c4d6a
mixelpixel <mixelpix@mac.com>
2017-06-25 15:58:18 -0400

    recursion

---
+++ ch03-functions/03.09b.js
@@ -0,0 +1,16 @@
+// http://eloquentjavascript.net/03_functions.html#p_KNlnW1VpW2
+
+function findSolution(target) {
+  function find(start, history) {
+    if (start == target)
+      return history;
+    else if (start > target)
+      return null;
+    else
+      return find(start + 5, "(" + history + " + 5)") ||
+             find(start * 3, "(" + history " * 3)");
+  }
+  return find(1, "1");
+}
+
+console.log(findSolution(24)); // <--- (((1 * 3) + 5) * 3)
