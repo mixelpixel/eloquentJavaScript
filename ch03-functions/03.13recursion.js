227b4d58858808aa1e74fba9f888f2a8a629b509
mixelpixel <mixelpix@mac.com>
2017-06-25 19:24:31 -0400

    positive or negative integers

--- ch03-functions/03.13recursion.js
+++ ch03-functions/03.13recursion.js
@@ -13,7 +13,40 @@
 // }
 
 // version 2
+// function isEven(number) {
+//   if (number == 0)
+//     return true;
+//   else if (number == 1)
+//     return false;
+//   else
+//     return isEven(number - 2);
+// }
+
+// version 3 - handling negative numbers
+// function isEven(number) {
+//   if (number < 0)
+//     if (number == 0)
+//       return true;
+//     else if (number == -1)
+//       return false;
+//     else
+//       return isEven(number + 2);
+//   else if (number >= 0)
+//     if (number == 0)
+//       return true;
+//     else if (number == 1)
+//       return false;
+//     else
+//       return isEven(number - 2);
+// }
+
+// There's got to be a more elegant solution ...?
+
+// version 4 - handling negative numbers
 function isEven(number) {
+  if (number < 0)
+    // number = -number
+    number *= -1
   if (number == 0)
     return true;
   else if (number == 1)
@@ -22,5 +55,7 @@ function isEven(number) {
     return isEven(number - 2);
 }
 
-console.log(isEven(50)); // <-- true
-console.log(isEven(75)); // <-- false
+console.log(isEven(50)); // <---- true
+console.log(isEven(75)); // <---- false
+console.log(isEven(-1)); // <---- false
+console.log(isEven(-1234));// <-- true
