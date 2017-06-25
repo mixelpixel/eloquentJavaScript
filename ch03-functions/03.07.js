78e4a8bbd397202840d2200a99e6ce9ee18ef880
mixelpixel <mixelpix@mac.com>
2017-06-25 14:14:00 -0400

    optional arguments ignored

---
+++ ch03-functions/03.07.js
@@ -0,0 +1,4 @@
+// http://eloquentjavascript.net/03_functions.html#h_1pGtRjrCUp
+
+// Entered into the browser console, the latter two arguments are simply ignored
+// alert("Hello", "Good Evening", "How do you do?");
