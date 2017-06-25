6b1124a01d04e200de2092b0959b96dfae9f4a84
mixelpixel <mixelpix@mac.com>
2017-06-25 12:33:10 -0400

    raise a base number to the power of an exponent

--- ch03-functions/03.01c.js
+++ ch03-functions/03.01c.js
@@ -1,30 +1,30 @@
 // http://eloquentjavascript.net/03_functions.html#c_YeYw47ylC5
 
-// var power = function(base, exponent) {
-//     var result = 1;
-//     for (var count = 0; count < exponent; count++)
-//       result *= base;
-//     return result;
-// };
-//
-// console.log(power(2, 10)); // <-- 1024
+var power = function(base, exponent) {
+    var result = 1;
+    for (var count = 0; count < exponent; count++)
+      result *= base;
+    return result;
+};
+
+console.log(power(2, 10)); // <-- 1024
 
 // prompt the user for a base and an exponent
 // return the result of the base to the power of the exponent
 
 // nodejs process to get user input
 //per: https://stackoverflow.com/a/32276364/5225057
-function prompt(question, callback) {
-  var stdin  = process.stdin,
-      stdout = process.stdout;
-
-  stdin.resume();
-  stdout.write(question);
-
-  stdin.once('data', function(data) {
-    callback(data.toString().trim());
-  });
-};
+// function prompt(question, callback) {
+//   var stdin  = process.stdin,
+//       stdout = process.stdout;
+//
+//   stdin.resume();
+//   stdout.write(question);
+//
+//   stdin.once('data', function(data) {
+//     callback(data.toString().trim());
+//   });
+// };
 
 // Use case
 // prompt('What\'s your name? ', function (input) {
