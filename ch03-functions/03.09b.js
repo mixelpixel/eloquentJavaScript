4110a962c2a0364cfba5afdc97f017a41248487b
mixelpixel <mixelpix@mac.com>
2017-06-25 16:00:22 -0400

    recursion

--- ch03-functions/03.09b.js
+++ ch03-functions/03.09b.js
@@ -8,7 +8,7 @@ function findSolution(target) {
       return null;
     else
       return find(start + 5, "(" + history + " + 5)") ||
-             find(start * 3, "(" + history " * 3)");
+             find(start * 3, "(" + history + " * 3)");
   }
   return find(1, "1");
 }
