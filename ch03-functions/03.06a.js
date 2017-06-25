cf9ef42639ff5bebad174d6d3162cb283987a7c2
mixelpixel <mixelpix@mac.com>
2017-06-25 13:56:38 -0400

    basic flow through the call stack

--- ch03-functions/03.06a.js
+++ ch03-functions/03.06a.js
@@ -1,7 +1,15 @@
 // http://eloquentjavascript.net/03_functions.html#h_D2Yui+mx6D
 
+// function greet(who) {
+//   console.log("Hello " + who);
+// }
+// greet("Harry");
+// console.log("Bye.");
+
+greet("Harry");
+
 function greet(who) {
   console.log("Hello " + who);
 }
-greet("Harry");
+
 console.log("Bye.");
