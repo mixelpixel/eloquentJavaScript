2027b18e0880606ae1387a3fc72b12d81cbc4ce8
mixelpixel <mixelpix@mac.com>
2017-06-26 13:50:37 -0400

    delete & in

---
+++ ch04-data-structures_objects-arrays/04.05b.js
@@ -0,0 +1,8 @@
+// http://eloquentjavascript.net/04_data.html#p_0r+HcrZzZJ
+
+var anObject = {left: 1, right: 2};
+console.log(anObject.left); //       ---> 1
+delete anObject.left;
+console.log(anObject.left); //       ---> undefined
+console.log("left" in anObject); //  ---> false
+console.log("right" in anObject); // ---> true
