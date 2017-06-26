a02d9efd18aa2d9fe336bd64a9f9127a828276cd
mixelpixel <mixelpix@mac.com>
2017-06-26 15:04:51 -0400

    counting letters

--- ch03-functions/03.13bean_counting.js
+++ ch03-functions/03.13bean_counting.js
@@ -37,3 +37,24 @@ function countChar(string, letter) {
 }
 
 console.log(countChar("kakkerlak", "k")); // <--- 4
+
+/*
+Answer per author: http://eloquentjavascript.net/code/#3.3
+
+function countChar(string, ch) {
+  var counted = 0;
+  for (var i = 0; i < string.length; i++)
+    if (string.charAt(i) == ch)
+      counted += 1;
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
+*/
