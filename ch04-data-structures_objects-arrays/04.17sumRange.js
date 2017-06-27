bb741af446c0bdf5fff6c45d976410afb7da2be2
mixelpixel <mixelpix@mac.com>
2017-06-27 18:59:30 -0400

    Sum range and bonus step range

--- ch04-data-structures_objects-arrays/04.17sumRange.js
+++ ch04-data-structures_objects-arrays/04.17sumRange.js
@@ -1,5 +1,6 @@
 // http://eloquentjavascript.net/04_data.html#h_8ZspxiCEC/
-function range (start, end) {
+
+function range(start, end) {
   rangeArray = new Array;
   for (var i = start; i <= end; i++) {
     rangeArray.push(i);
@@ -9,12 +10,33 @@ function range (start, end) {
 
 console.log(range(1, 10)); // ---> [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 
-function sum (anArray) {
-  sum = 0;
+function arraySum(anArray) {
+  arraySum = 0;
   for (var i = 0; i <= anArray.length; i++) {
-    sum +=i;
+    arraySum +=i;
   }
-  return sum;
+  return arraySum;
 }
 
-console.log(sum(range(1, 10))); // ---> 55
+// console.log(arraySum(range(1, 10))); // ---> 55
+// console.log(arraySum(range(1, 10))); // ---> 55
+
+function stepRange(start, end, step) {
+  if (step === undefined)
+    step = 1;
+  stepRangeArray = new Array;
+  if (step < 0) {
+    for (var i = start; i >= end; i += step) {
+      stepRangeArray.push(i);
+    } return stepRangeArray;
+  } else if (step > 0) {
+    for (var i = start; i <= end; i += step) {
+      stepRangeArray.push(i);
+    } return stepRangeArray;
+  } else return start;
+}
+
+console.log(stepRange(1, 10));      // ---> [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(stepRange(5, 2, -1));   // ---> [ 5, 4, 3, 2 ]
+console.log(stepRange(1, 10, 2));   // ---> [ 1, 3, 5, 7, 9 ]
+// console.log(arraySum(stepRange(1, 10))); // ---> 55
